3d3b24d280a0b30890b03bb6c975165f5954b379
ullas <ullaskunder3@gmail.com>
2021-12-13 23:27:33 +0530

    Project sprite

---
+++ README.md
@@ -0,0 +1,20 @@
+# Anime Sprite Animation
+
+Sprite Animation comes from the classic paper flip animation concept.
+In Sprite Animation, two or more images get rendered sequentially in a continuous manner which, in turn, gives an animated single object.
+
+Example:
+
+![example](./example.gif)
+
+---
+
+- A sprite sheet consists of multiple frames (also sprites) in one image.
+
+- In other words, sprite sheets pack multiple sprites into a single picture.
+
+- Using sprite sheet, you create sprite sheet animation representing one or - several animation sequences while only loading a single file.
+
+- Sprite sheet animation changes which frame/sprite is rendered in quick succession to give the illusion of movement.
+
+![sheet](./img/sword.png)
---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('#canvas_container')
+const ctx = canvas.getContext('2d');
+
+const spriteWidth = 319
+const spriteHeight = 329
+
+const CANVAS_WIDTH = canvas.width = 400
+const CANVAS_HEIGHT = canvas.height = 400
+
+const playerImg = new Image();
+// console.log(playerImg);
+
+playerImg.src = './img/sword.png';
+let frameX = 0;
+let frameY = 0;
+let initFrame = 0;
+const staggerFrame = 7;
+// console.log(staggerFrame, initFrame);
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    let position = Math.floor(initFrame/staggerFrame) % 9;
+    frameX = spriteWidth* position;
+
+    console.log(position);
+
+    ctx.drawImage(playerImg, frameX, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+    initFrame++;
+    requestAnimationFrame(animate)
+}
+animate()
